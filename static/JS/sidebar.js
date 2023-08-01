1860b1e947d6eeb84ea27857d52d58393c14367b
navansh03 <119163544+navansh03@users.noreply.github.com>
2023-08-01 20:19:42 +0530

    tilt block and routing

--- static/JS/sidebar.js
+++ static/JS/sidebar.js
@@ -32,9 +32,33 @@ updateTypedStrings();
 
 
 
+$(window).bind('orientationchange resize', function(event){
+  if (event.orientation) {
+    if (event.orientation == 'landscape') {
+      if (window.rotation == 90) {
+        rotate(this, -90);
+      } else {
+        rotate(this, 90);
+      }
+    }
+  }
+});
 
-
-
+function rotate(el, degs) {
+  iedegs = degs/90;
+  if (iedegs < 0) iedegs += 4;
+  transform = 'rotate('+degs+'deg)';
+  iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
+  styles = {
+    transform: transform,
+    '-webkit-transform': transform,
+    '-moz-transform': transform,
+    '-o-transform': transform,
+    filter: iefilter,
+    '-ms-filter': iefilter
+  };
+  $(el).css(styles);
+}
 window.onload = function() {
 var video = document.getElementById('myVideo');
 var startTime = 5; 
